af12906244a7b70a7a31ba1f47d2917f77dc15d2
masterstrawberry <jvillacis@my.yearupunited.org>
2026-04-21 10:11:00 -0400

    update

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,10 +6,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         try {
-            FileReader fileReader = new FileReader("../DataFiles/employees.csv");
+            Scanner scanner =  new Scanner(System.in);
+            System.out.println("enter file to process: ");
+            String file = scanner.nextLine();
+            System.out.println("enter the file to create: ");
+            String file1 = scanner.nextLine();
+
+            FileReader fileReader = new FileReader("../DataFiles/"+file);
+            FileWriter writer = new FileWriter(file1);
+
             // create a FileReader object connected to the File
             // create a BufferedReader to manage input stream
             BufferedReader bufReader = new BufferedReader(fileReader);
+            BufferedWriter bufWriter = new BufferedWriter(writer);
             String input;
             // read until there is no more data
             bufReader.readLine();
@@ -17,10 +26,10 @@ public class Main {
                 String []arr = input.split("\\|");
                 Employee employee = new Employee(Integer.parseInt(arr[0]), arr[1], Double.parseDouble(arr[2]), Double.parseDouble(arr[3]));
 
-                System.out.println("\n");
-                System.out.println("employeeId: " + employee.getEmployeeId());
-                System.out.println("name: " + employee.getName());
-                System.out.println("gross pay: " + employee.getGrossPay());
+                bufWriter.write("\n");
+                bufWriter.write("employeeId: " + employee.getEmployeeId());
+                bufWriter.write("name: " + employee.getName());
+                bufWriter.write("gross pay: " + employee.getGrossPay());
 
             }
             // close the stream and release the resources
